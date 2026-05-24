bb438f33093a7c943ca958425a81b5a5a94d5e11
Priyanka Chahal <priyanka@192.168.1.27>
2026-05-24 14:38:54 +0530

    adding building jar files in docker files of all services

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ecommerce_server" {
 
-  ami           = "ami-0f58b397bc5c1f2e8"
-  instance_type = "t2.micro"
+  ami           = "ami-05cf1e9f73fbad2e2"
+  instance_type = "t2.medium"
 
   key_name = "ecommerce-key"
 
